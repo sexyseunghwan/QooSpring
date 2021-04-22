4fb9151ceeec02aa46dd7fc53427020e275125f7
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-04-22 23:03:44 +0900

    2021-04-22

--- SpringTest/src/main/java/com/test/spring/AOPController.java
+++ SpringTest/src/main/java/com/test/spring/AOPController.java
@@ -3,7 +3,7 @@ package com.test.spring;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,8 +13,8 @@ public class AOPController {
 	
 	
 	//스프링이 자동으로 의존주입 -> 자동으로 객체만들어서 넣어준다. 
-	@Autowired
-	private IMyBatisService service;
+	//@Autowired
+	//private IMyBatisService service;
 	
 	//주업무
 	@RequestMapping(value = "/index.action", method = { RequestMethod.GET })
@@ -23,11 +23,28 @@ public class AOPController {
 		//시작 페이지
 		//int count = service.getCount();
 		System.out.println("==============");
-		System.out.println(service.m1());
+		//System.out.println(service.m1());
 		System.out.println("==============");
 		//request.setAttribute("count", count);
-
+		
+		
+		
 		return "result";//result.jsp 를 부르게 된다.
+	}
+	
+	@RequestMapping(value = "/index1.action", method = { RequestMethod.GET })
+	public String index2(HttpServletRequest request, HttpServletResponse response) {
+
+		//시작 페이지
+		//int count = service.getCount();
+		System.out.println("==============");
+		//System.out.println(service.m1());
+		System.out.println("==============");
+		//request.setAttribute("count", count);
+		
+		
+		
+		return "result1";//result.jsp 를 부르게 된다.
 	}
 	
 	
